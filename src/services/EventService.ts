3a0f920abb50fb7fbcdab3174831ed368692e54c
nureman <nureman_s@cmu.ac.th>
2023-08-07 09:02:21 +0700

    add hosting

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,20 +1,22 @@
-import axios from 'axios'
-import type { AxiosInstance, AxiosResponse } from 'axios'
-import type { EventItem } from '@/type'
+import axios from "axios";
+import type { AxiosInstance, AxiosResponse } from "axios";
+import type { EventItem } from "@/type";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3004',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: false,
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
-})
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
 export default {
   getEvent(perPage: number, page: number): Promise<AxiosResponse<EventItem[]>> {
-    return apiClient.get<Array<EventItem>>('/events?_limit=' + perPage + '&_page=' + page)
+    return apiClient.get<Array<EventItem>>(
+      "/events?_limit=" + perPage + "&_page=" + page
+    );
   },
   getEventById(id: number): Promise<AxiosResponse<EventItem>> {
-    return apiClient.get<EventItem>('events/' + id.toString())
-  }
-}
+    return apiClient.get<EventItem>("events/" + id.toString());
+  },
+};
